e596de86240ab10713e2130a51a8a9c28d2974f0
İsmail ŞEKER <sseker665@gmail.com>
2025-01-18 21:12:34 +0300

    implement with keras library

--- ANN.py
+++ ANN.py
@@ -94,6 +94,25 @@ def two_layer_neural_network(x_train, y_train,x_test,y_test, num_iterations):
 
 parameters = two_layer_neural_network(x_train, y_train,x_test,y_test, num_iterations=2500)
 
+# implementation with keras library
+
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from keras.models import Sequential # initialize neural network library
+from keras.layers import Dense # build our layers library
+def build_classifier():
+    classifier = Sequential() # initialize neural network
+    classifier.add(Dense(units = 8, kernel_initializer = 'uniform', activation = 'relu', input_dim = x_train.shape[1]))
+    classifier.add(Dense(units = 4, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier
+classifier = KerasClassifier(build_fn = build_classifier, epochs = 100)
+accuracies = cross_val_score(estimator = classifier, X = x_train, y = y_train, cv = 3)
+mean = accuracies.mean()
+variance = accuracies.std()
+print("Accuracy mean: "+ str(mean))
+print("Accuracy variance: "+ str(variance))
 
 
 
